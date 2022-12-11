8f1dec8dfabd08bd4b13c076c612e2b2b422ead7
Heidrun Kubiessa <heidrunkubiessa@heidruns-air.lan>
2022-12-11 16:36:05 -0700

    finished models and routes, almost finished README

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,26 +3,117 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  // including its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  // including its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product}]
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+  }
+  res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update({
+      where: {
+        id: req.params.id
+      }
+    });
+//     .then((category) => {
+//       // find associated tag from Product
+//       return Product.findAll({ where: { category_id: req.params.id } });
+//     })
+//     .then((product) => {
+//       // get list of current product_ids
+//       const productIds = product.map(({ product_id }) => product_id);
+//       // create filtered list of new product_ids
+//       const newProduct = req.body.productIds
+//         .filter((product_id) => !productIds.includes(product_id))
+//         .map((product_id) => {
+//           return {
+//             category_id: req.params.id,
+//             product_id,
+//           };
+//         });
+//       // figure out which ones to remove
+//       const productToRemove = productTags
+//         .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+//         .map(({ id }) => id);
+
+//       // run both actions
+//       return Promise.all([
+//         ProductTag.destroy({ where: { id: productTagsToRemove } }),
+//         ProductTag.bulkCreate(newProductTags),
+//       ]);
+//     })
+//     .then((updatedProductTags) => res.json(updatedProductTags))
+//     .catch((err) => {
+//       // console.log(err);
+//       res.status(400).json(err);
+//     });
+// });;
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# E-commerce Back End Starter Code
+# E-Commerce Backend App
+## Description
+This is a backend application for an e-commerce site that uses Sequelize to interact with a MySQL database. Insomnia is used to interact with the backend to view the products, categories, and tags, as well as use CRUD methods to alter the database.
+
+## Installation 
+Clone the repo onto your local machine, download Insomnia  and watch the demo video to get the app started. 
+
+## Usage 
+
+### User Story
+AS A manager at an internet retail company
+I WANT a back end for my e-commerce website that uses the latest technologies
+SO THAT my company can compete with other e-commerce companies
+
+
+### Acceptance Criteria 
+GIVEN a functional Express.js API
+WHEN I add my database name, MySQL username, and MySQL password to an environment variable file
+THEN I am able to connect to a database using Sequelize
+WHEN I enter schema and seed commands
+THEN a development database is created and is seeded with test data
+WHEN I enter the command to invoke the application
+THEN my server is started and the Sequelize models are synced to the MySQL database
+WHEN I open API GET routes in Insomnia Core for categories, products, or tags
+THEN the data for each of these routes is displayed in a formatted JSON
+WHEN I test API POST, PUT, and DELETE routes in Insomnia Core
+THEN I am able to successfully create, update, and delete data in my database
+
+## Mockup 
+![]
+
+## Technologies 
+- JavaScript
+  -  Node
+  - Express
+  - JSON
+  - Sequelize 
+- MySQL
+- dotenv
+- Git and Github
+
+## Questions 
+Please contact me at: https://github.com/Kubikubiessa or kubikubiessa@gmail.com
+
+## License 
+MIT 
+
+
